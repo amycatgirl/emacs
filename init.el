b0d629aca65b53510e73aabc6a6fa609cc28de78
Amy <amy@amogus.cloud>
2025-06-09 07:35:27 -0400

    oops

--- init.el
+++ init.el
@@ -1,28 +1,3 @@
-!/modules/*;; Amy's minimal emacs configuration
-;; Authored: Jun 8 2025
-
-; Utilities
-(defun amy/load-module (name)
-  "Loads a configuration module. The argument NAME should be a valid module name
-   in \"<user-emacs-directory>/modules/\"."
-  (load (expand-file-name (symbol-name name) (concat user-emacs-directory "modules/"))))
-
-;; Core configuration
-(amy/load-module 'core)
-
-;; Completion Module
-(amy/load-module 'comp)
-
-;; IDE Modules
-(amy/load-module 'prog)
-(amy/load-module 'lisp)
-
-;; Text editing
-(amy/load-module 'org)
-
-; Visual Flair
-(amy/load-module 'ui)
-(amy/load-module 'theme)
 ;; Amy's minimal emacs configuration
 ;; Authored: Jun 9 2025
 
