d207773977bd1185ec129c11e5162ac8075d9129
Amy <amy@amogus.cloud>
2025-06-15 09:41:43 -0400

    Update lsp module to use lsp-mode instead of eglot

--- modules/lsp.el
+++ modules/lsp.el
@@ -1,3 +1,18 @@
 ; LSP
-(use-package eglot
-  :hook (prog-mode . eglot-ensure))
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((typescript-ts-mode . lsp)
+	 (csharp-ts-mode . lsp)
+	 (python-ts-mode . lsp))
+  :commands lsp)
+
+(use-package lsp-ui
+  :after lsp-mode
+  :custom
+  (lsp-ui-sideline-show-diagnostics t)
+  (lsp-ui-sideline-show-hover t))
+
+(use-package flycheck
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
